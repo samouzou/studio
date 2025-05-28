44e7bf62b47fd6d26c50d46760deb1592623b361
Serge Amouzou <serge@datatrixs.com>
2025-05-28 21:26:42 +0000

    updaing time stamp to firebase server

--- functions/src/payments/index.ts
+++ functions/src/payments/index.ts
@@ -315,7 +315,7 @@ export const handlePaymentSuccess = onRequest(async (request, response) => {
       // Update contract status
       await db.collection("contracts").doc(contractId).update({
         invoiceStatus: "paid",
-        updatedAt: new Date(),
+        updatedAt: admin.firestore.FieldValue.serverTimestamp(),
         invoiceHistory: admin.firestore.FieldValue.arrayUnion({
           timestamp: admin.firestore.FieldValue.serverTimestamp(),
           action: "Invoice Paid",
@@ -374,7 +374,7 @@ export const handlePaymentSuccess = onRequest(async (request, response) => {
         customerId: customer,
         emailForUserConfirmation,
         status: "succeeded",
-        timestamp: new Date(),
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
       });
     }
     response.json({received: true});
